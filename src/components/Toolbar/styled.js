597e4d63a6d2edac54d0e33f1db19d197223de27
MaxMitko <maxmitko@mail.ru>
2019-04-30 17:18:22 +0300

    chore(components): remove unnecessary code

--- src/components/Toolbar/styled.js
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-export const ToolbarStyled = styled.div`
-  padding: 0 24px;
-  min-height: 64px;
-  display: flex;
-  position: relative;
-  align-items: center;
-`;
